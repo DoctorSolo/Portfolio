f67032a2ed2b1ab6940a2848cdb6a333f080ac42
Miguel Eduardo da Silva <miguelodoutor@gmail.com>
2026-08-02 17:18:07 -0300

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import fundo from "./assets/fundo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-fixed bg-cover bg-center bg-[url('./assets/fundo.jpg')]">
+      Portfolio
+    </div>
+  );
+}
+
+export default App;
